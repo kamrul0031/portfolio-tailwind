64b192893206ac24ae23945e491228dd406690cc
kamrul0031 <muhammadkamrulhasantamim@gmai.com>
2024-02-25 16:08:42 +0600

    up

---
+++ packages/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {},
+    fontFamily:{
+      abc: ["Kode Mono", monospace],
+    },
+  },
+  plugins: [],
+}
+
